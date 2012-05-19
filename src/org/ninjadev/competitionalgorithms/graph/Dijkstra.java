8cb60ec142664285d1ea5ffbd9fc98949cace088
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2012-05-19 03:00:42 +0200

    Added adjacency matrix implementation of Dijkstra

---
+++ src/org/ninjadev/competitionalgorithms/graph/Dijkstra.java
@@ -0,0 +1,99 @@
+package org.ninjadev.competitionalgorithms.graph;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class Dijkstra {
+	
+	/*
+	 * The Node class for Dijkstra.
+	 */
+	public static class Node implements Comparable<Node>{
+		public int dist;
+		public int index;
+		public int prev;
+		public boolean done;
+		
+		public Node(int index){
+			this.dist = Integer.MAX_VALUE;
+			this.index = index;
+			this.prev = -1;
+			this.done = false;
+		}
+		
+		public int compareTo(Node n) {
+			return dist-n.dist;
+		}
+	}
+	
+	/*
+	 * Dijkstra's algorithm for graphs represented by an adjacency matrix.
+	 */
+	public static Node[] dijkstra(int[][] graph, int source){
+		
+		/* declare an array of nodes */	
+		Node[] nodes = new Node[graph.length];
+		
+		/* init the nodes */
+		for(int i=0;i<nodes.length;i++){
+			nodes[i] = new Node(i);
+		}
+		
+		/* distance from source to source */
+		nodes[source].dist  = 0;
+		
+		/* declare the priority queue to hold the nodes to be inspected */
+		PriorityQueue<Node> Q = new PriorityQueue<Node>();
+		
+		/* fill Q with all the nodes */
+		Q.addAll(Arrays.asList(nodes));
+		
+		/* main loop */
+		while(!Q.isEmpty()){
+			
+			/* get the node nearest to the source */
+			Node u = Q.poll();
+			u.done = true;
+			
+			/* if the nearest node is unreachable, we are done */
+			if(u.dist == Integer.MAX_VALUE){
+				break;
+			}
+			
+			/* for each neighbor v of u */
+			for(Node v : nodes){
+				
+				/* continue with next node if v is marked as done */
+				if(v.done){
+					continue;
+				}
+				
+				/* continue with next node if v is not actually a neighbor */
+				if(graph[u.index][v.index] == Integer.MAX_VALUE){
+					continue;
+				}
+				
+				/* calculate shortest distance to v from source through u */
+				int alt = u.dist + graph[u.index][v.index]; 
+				
+				/* if we have found a shorter path from source to v */
+				if(alt < v.dist){
+					
+					/* update the v node */
+					v.dist = alt;
+					v.prev = u.index;
+					
+					/* reorder v in the queue */
+					Q.remove(v);
+					Q.add(v);
+				}
+			}
+			
+			/* mark the node as done */
+			u.done = true;
+		}
+		
+		/* finally return the array of nodes */
+		return nodes;
+	}
+}
---
+++ tests/org/ninjadev/competitionalgorithms/tests/graph/DijkstraTest.java
@@ -0,0 +1,82 @@
+package org.ninjadev.competitionalgorithms.tests.graph;
+
+import org.ninjadev.competitionalgorithms.graph.Dijkstra;
+import org.ninjadev.competitionalgorithms.graph.Dijkstra.Node;
+
+import junit.framework.TestCase;
+
+
+public class DijkstraTest extends TestCase {
+	
+	/* short form for "infinity" */
+	static final int i = Integer.MAX_VALUE;
+	
+	public static void main(String[]args){
+		DijkstraTest.testEmptyGraph();
+		DijkstraTest.testMonoweightedGraph();
+		DijkstraTest.testHandMadeGraph();
+	}
+	
+	public static void testEmptyGraph(){
+		int[][] graph = {
+				{i,i,i,i,i,i,i,i},
+				{i,i,i,i,i,i,i,i},
+				{i,i,i,i,i,i,i,i},
+				{i,i,i,i,i,i,i,i},
+				{i,i,i,i,i,i,i,i},
+				{i,i,i,i,i,i,i,i},
+				{i,i,i,i,i,i,i,i},
+				{i,i,i,i,i,i,i,i}
+				};
+		
+		int source = 0;
+		Node[] nodes = Dijkstra.dijkstra(graph, source);
+		
+		for(Node n : nodes){
+			assertEquals(n.index == source ? 0 : i, n.dist);
+		}
+	}
+	
+	public static void testMonoweightedGraph(){
+		int[][] graph = {
+				{0,1,1,1,1,1,1,1},
+				{1,0,1,1,1,1,1,1},
+				{1,1,0,1,1,1,1,1},
+				{1,1,1,0,1,1,1,1},
+				{1,1,1,1,0,1,1,1},
+				{1,1,1,1,1,0,1,1},
+				{1,1,1,1,1,1,0,1},
+				{1,1,1,1,1,1,1,0}
+				};
+		
+		int source = 0;
+		Node[] nodes = Dijkstra.dijkstra(graph, source);
+		
+		for(Node n : nodes){
+			assertEquals(n.index == source ? 0 : 1, n.dist);
+		}
+	}
+	
+	public static void testHandMadeGraph(){
+		int[][] graph = {
+				{0,2,4,i,i,i,i},
+				{2,0,3,i,i,6,i},
+				{4,3,0,6,i,i,i},
+				{i,i,6,0,2,i,3},
+				{i,i,i,2,0,i,5},
+				{i,6,i,i,i,0,9},
+				{i,i,i,3,5,9,0}
+				};
+		
+		int source = 1;
+		
+		Node[] nodes = Dijkstra.dijkstra(graph, source);
+		
+		int[] expectedDistance = { 2, 0, 3, 9, 11, 6, 12};
+		
+		for(Node n : nodes){
+			assertEquals(expectedDistance[n.index], n.dist);
+		}
+	}
+}
+		
